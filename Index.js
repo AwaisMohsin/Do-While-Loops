2adf87b63ef20f970d72beacf89d8ed15871f220
Awais <Awaisbinmohsin@gmail.com>
2022-05-27 00:43:56 +0500

    Do While Loops

---
+++ Index.js
@@ -0,0 +1,17 @@
+// By writing while and do while you will understand in better way
+
+//while loop
+
+let a = 0;
+while( a<= 100){
+console.log(a);
+a++;
+}
+
+//do while
+
+let b = 100;
+do{
+    console.log(b);
+    b--;
+} while (b>=0);
